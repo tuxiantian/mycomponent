2f87ca78bbe6d594558eb5cd9b8415f18830c07d
tuxiantian <tuxiantian@163.com>
2016-08-29 11:39:25 +0800

    清除无用代码

--- src/servlet/JqueryFormServlet.java
+++ src/servlet/JqueryFormServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.net.URLDecoder;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import sun.misc.BASE64Decoder;
 import util.Des;
 
 /**
@@ -29,7 +31,6 @@ public class JqueryFormServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	}
 
 	/**
